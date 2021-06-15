50cce3ba4888f9f355bf7d35101b35f870dc5e4c
Cnidarian <cmphillips23@gmail.com>
2021-06-15 01:52:35 -0500

    temp populates in cards after city search

--- assets/index.js
+++ assets/index.js
@@ -1,23 +1,42 @@
 var city = document.getElementById('city-input');
+var dailyOne = document.getElementById('card-temp-1');
+var dailyTwo = document.getElementById('card-temp-2');
+var dailyThree = document.getElementById('card-temp-3');
+var dailyFour = document.getElementById('card-temp-4');
+var dailyFive = document.getElementById('card-temp-5');
+
+var tempArr = [];
 
 function getForecast() {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=29.74439674904094&lon=-95.36559187196157&exclude=minutely,hourly,alerts&appid=f8f9b280883e596e240d7e0d55c206fb"
+    var forecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=29.74439674904094&lon=-95.36559187196157&exclude=minutely,hourly,alerts&appid=f8f9b280883e596e240d7e0d55c206fb"
 
     fetch(apiUrl);
 }
 
 function getLatLng() {
-    var apiUrl = "http://www.mapquestapi.com/geocoding/v1/address?key=UdymKn6A0FogSJTjNS96CNnnoR5ku5D6&location=" + city.value;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=f8f9b280883e596e240d7e0d55c206fb"
 
     fetch(apiUrl).then(function(response) {
-        if (response.ok) {
-            console.log(response);
-        } else {
-            alert("Error: " + response.statusText);
-        }
-
-        var latitude = response.results.locations.latLng.lat;
-        var longitude = response.results.locations.latLng.lng;
-        console.log(latitude, longitude);
+        return response.json().then(function(response) {
+            var latitude = response.coord.lat;
+            var longitude = response.coord.lon;
+            var forecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&appid=f8f9b280883e596e240d7e0d55c206fb";
+            fetch(forecastUrl).then(function(response) {
+                return response.json().then(function(response) {
+                    for (i = 0; i < 5; i++) {
+                        tempArr.push(response.daily[i].temp.day);
+                    }
+                    temp(tempArr);
+                })
+            })
+        })
     });
 };
+
+function temp() {
+    dailyOne.textContent = tempArr[0] + "°F";
+    dailyTwo.textContent = tempArr[1] + "°F";
+    dailyThree.textContent = tempArr[2] + "°F";
+    dailyFour.textContent = tempArr[3] + "°F";
+    dailyFive.textContent = tempArr[4] + "°F";
+}
